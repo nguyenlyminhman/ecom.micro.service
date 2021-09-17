e16b3a3b84c6ad78aa04d6378115e20b9827eee8
Man Nguyen <nguyenlyminhman@gmail.com>
2021-09-17 10:39:01 +0700

    Done service order

--- order_service/app.js
+++ order_service/app.js
@@ -1,17 +1,33 @@
 var express = require('express');
+const amqp = require('amqplib'); 
+
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+let connection, chanel;
+
+async function connectAmqp () {
+    
+    let amqpserver = 'amqp://localhost:5672'
+    connection = await amqp.connect(amqpserver);
+    chanel = await connection.createChannel();
+    await chanel.assertQueue('ORDER_QUEUE');
+}
+
+connectAmqp().then( ()=>{
+    chanel.consume('ORDER_QUEUE', (data)=>{
+      console.log(JSON.parse(data.content));  
+    })
+    
+});
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
